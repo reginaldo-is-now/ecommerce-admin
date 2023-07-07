4154ce4ed6b822bcaabaa6f23776aac9320547bd
New UsernameMarvin <reginaldo.is.now@gmail.com>
2023-07-07 14:52:25 +0800

    new update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="p-4">
-        <Button size="default" variant="outline">Click me</Button>
+        <Button>Click me</Button>
     </div>
   )
 }
